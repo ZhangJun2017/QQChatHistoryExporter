d4395b36c1935ebef95028a19ff88075012c121f
ZhangJun-ZhangJun_PC <sn2015.zhang@gmail.com>
2021-08-10 04:20:02 +0800

    Fix a bug in emotion replacement twice again

--- src/TextMessage.java
+++ src/TextMessage.java
@@ -7,14 +7,34 @@ public class TextMessage extends Message {
     }
 
     public static String escapeHtml(String toEscape) {
-        toEscape = toEscape.replace("&", "&amp;");
-        toEscape = toEscape.replace("\"", "&quot;");
-        toEscape = toEscape.replace("'", "&apos;");
-        toEscape = toEscape.replace(" ", "&nbsp;");
-        toEscape = toEscape.replace("<", "&lt;");
-        toEscape = toEscape.replace(">", "&gt;");
-        toEscape = toEscape.replace("\n", "<br>");
-        return toEscape;
+        if (!toEscape.contains("\u0014")) {
+            toEscape = toEscape.replace("&", "&amp;");
+            toEscape = toEscape.replace("\"", "&quot;");
+            toEscape = toEscape.replace("'", "&apos;");
+            toEscape = toEscape.replace(" ", "&nbsp;");
+            toEscape = toEscape.replace("<", "&lt;");
+            toEscape = toEscape.replace(">", "&gt;");
+            toEscape = toEscape.replace("\n", "<br>");
+            return toEscape;
+        } else {
+            if (toEscape.equals("\u0014\u0014")) {
+                return toEscape;
+            }
+            String[] toEscapeArray = toEscape.split("\u0014");
+            StringBuilder message = new StringBuilder(escapeHtml(toEscapeArray[0]));
+            for (int i = 0; i < toEscapeArray.length - 1; i++) {
+                message.append('\u0014');
+                if (toEscapeArray[i + 1].length() > 0) {
+                    message.append(toEscapeArray[i + 1].charAt(0));
+                    message.append(escapeHtml(toEscapeArray[i + 1].substring(1)));
+                } else {
+                    // a standalone \u0014 ??
+                    message.append('\u0034');
+                }
+            }
+            message.append(toEscapeArray.length < 3 || toEscapeArray[toEscapeArray.length - 1].length() < 2 ? "" : escapeHtml(toEscapeArray[toEscapeArray.length - 1]));
+            return message.toString();
+        }
     }
 
     @Override
